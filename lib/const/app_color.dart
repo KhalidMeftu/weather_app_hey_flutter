2d34ed521ded9513d009e5bfec77c05ecf00d745
Khalid Meftu <90188748+KhalidMeftu@users.noreply.github.com>
2023-11-10 11:39:19 +0300

    added important dependencies

---
+++ lib/const/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class WeatherAppColor{
+  static Color transParentColor=Colors.transparent;
+  static Color whiteColorWOpacity=const Color(0xffFFFFFF).withOpacity(0.25);
+  static Color whiteColor=const Color(0xffFFFFFF);
+  static Color colorForCard=const Color(0xffECECEC);
+
+}
---
+++ lib/const/common_widgets/saved_cities_card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutterweatherapp/const/app_color.dart';
+
+class SavedCitiesCard extends StatelessWidget {
+  const SavedCitiesCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: WeatherAppColor.transParentColor,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(radius));
+      ),
+
+    );
+  }
+}
